e1b324cfec2bc884ac929ff326fd302def9782d1
emjasmine <75378436+emjasmine@users.noreply.github.com>
2021-04-22 20:26:55 -0400

    corrected path

--- static/js/app.js
+++ static/js/app.js
@@ -1,5 +1,5 @@
 //Use the D3 library to read in samples.json.
- d3.json('../../../data/samples.json').then(function(data){
+ d3.json('../../data/samples.json').then(function(data){
      var sampleData = data;
      console.log(sampleData);
 
